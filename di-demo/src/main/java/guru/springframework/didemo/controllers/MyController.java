53165e25e149c6dc7547536cefee9aead183acda
Elena Kishchenko <elana.kishchenko@lazada.com>
2017-12-26 13:58:07 +0700

    di-demo

---
+++ di-demo/src/main/java/guru/springframework/didemo/controllers/MyController.java
@@ -0,0 +1,17 @@
+package guru.springframework.didemo.controllers;
+
+import guru.springframework.didemo.services.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController {
+
+    @Autowired
+    private GreetingService greetingService;
+
+    public String hello() {
+        greetingService.sayGreeting();
+        return "index";
+    }
+}
